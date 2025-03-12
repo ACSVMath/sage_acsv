b0ae16d1e1355c8a4bbbfd13f0d30ec45fe66d6d
Benjamin Hackl <devel@benjamin-hackl.at>
2025-03-12 13:12:32 +0100

    some cleanups and refactoring in whitney.py

--- sage_acsv/whitney.py
+++ sage_acsv/whitney.py
@@ -1,23 +1,24 @@
 from sage.all import Ideal, PolynomialRing, ProjectiveSpace, QQ
 from sage.all import Combinations, matrix
 
-def PrimaryDecompositionM2(Id, m2):
-    """
-    Uses Macaulay2 to compute the primary decomposition of an ideal
-    """
-
-    Id = m2.ideal(Id)
-    for J in Id.decompose():
-        yield J.sage()
-
 def PrimaryDecomposition(Id, m2=None):
+    """Return the primary decomposition of an ideal.
+
+    If a Macaulay2 interface is provided, it will be used instead
+    of Sage's default implementation.
+    
+    INPUT:
+
+    * ``Id`` - A polynomial ideal
+    """
     if m2 is not None:
-        return PrimaryDecompositionM2(Id, m2)
+        return iter(J.sage() for J in m2.ideal(Id).decompose())
     return Id.primary_decomposition()
 
 def Con(X, P, RZ):
-    r"""
-    Computes the ideal associated with Con(X), which is the map that sends X to its conormal space
+    r"""Compute the ideal associated with the map sending `X` to its conormal space.
+
+    The map is also denoted as `\operatorname{Con}(X)`.
     """
     vs = P.gens()
     vzs = tuple(v for v in RZ.gens() if v not in vs)
@@ -31,14 +32,9 @@ def Con(X, P, RZ):
     return (X.defining_ideal().change_ring(RZ)+JacZ).saturation(Jac.change_ring(RZ))[0]
 
 def Decompose(Y,X,P,R,RZ, m2=None):
-    r"""
-    Given varieties X and Y, returns the points in Y that fail Whitney's condition B
-    with respect to X
+    r"""Given varieties `X` and `Y`, return the points in `Y` that fail
+    Whitney's condition `B` with respect to `X`.
     """
-    vs = P.gens()
-    vzs = tuple(v for v in RZ.gens() if v not in vs)
-    CX = Ideal(vs)
-
     d = Y.dimension()
     Ys = [P.subscheme(1) for _ in range(d+1)]
     Ys[-1] = Y
@@ -53,13 +49,12 @@ def Decompose(Y,X,P,R,RZ, m2=None):
             Ys[i] = Ys[i].union(W)
     return Ys
 
-def Merge(Xs, Ys):
-    r"""
-    Simple subroutine to merge two stratifications
+def merge_stratifications(Xs, Ys):
+    r"""Merge two stratifications.    
     """
     # Ensures Xs > Ys
     if len(Ys) > len(Xs):
-        return Merge(Ys, Xs)
+        return merge_stratifications(Ys, Xs)
     
     res = []
     for i in range(len(Ys)):
@@ -75,9 +70,7 @@ def WhitneyStratProjective(X, P, m2=None):
     """
     X = P.subscheme(X.defining_ideal().radical())
     vs = P.gens()
-    n = len(vs)-1
     k = X.dimension()
-    ck = X.codimension()
 
     R = P.gens()[0].parent()
     RZ = PolynomialRing(R, ['t%d'%i for i in range(len(vs))])
@@ -97,27 +90,27 @@ def WhitneyStratProjective(X, P, m2=None):
         Xd = Xs[d]
         if Xd.dimension() < d:
             continue
-        Xs = Merge(Xs, Decompose(Xd, X, P, R, RZ, m2))
-        Xs = Merge(Xs, WhitneyStratProjective(Xd, P, m2))
+        Xs = merge_stratifications(Xs, Decompose(Xd, X, P, R, RZ, m2))
+        Xs = merge_stratifications(Xs, WhitneyStratProjective(Xd, P, m2))
         
     return Xs
 
 def WhitneyStrat(IX, R, m2=None):
-    r"""
-    Computes the Whitney Stratification of a pure-dimensional algebraic variety. Uses
-    an algorithm developed by Helmer and Nanda (2022).
+    r"""Computes the Whitney Stratification of a pure-dimensional algebraic variety.
+
+    Uses an algorithm developed by Helmer and Nanda (2022).
 
     INPUT:
 
-    * ``IX`` - A `k`-dimensional polynomial ideal representation of the algebraic variety `X`
-    * ``R`` - Base ring of the ideal. Should be a PolynomialRing object
+    * ``IX`` -- A `k`-dimensional polynomial ideal representation of the algebraic variety `X`
+    * ``R`` -- Base ring of the ideal. Should be a PolynomialRing object
     * ``m2`` -- (Optional) The option to pass in a SageMath Macaulay2 interface for
         computing primary decompositions. Macaulay2 must be installed by the user
 
     OUTPUT:
 
-    A list `[IX_0, IX_1, ..., IX_k]` of polynomial ideals representing the Whitney stratification of `X`.
-    `IX_j` reprensents the `j`-dimensional stratum.
+    A list ``[IX_0, IX_1, ..., IX_k]`` of polynomial ideals representing the Whitney stratification of `X`.
+    ``IX_j`` reprensents the `j`-dimensional stratum.
 
     EXAMPLES::
 
